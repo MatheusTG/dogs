832015a926de840b6cba39503a8ccfc54a1bd6be
Matheus <matheusteodorogarcia@gmail.com>
2024-05-05 12:37:33 -0300

    Initial website structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Login from './components/Login/Login';
 
 const App = () => {
-  return <div>App React</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 };
 
 export default App;
