fd3dba3a3ce49d3b68de2ce22e21ccea0b24049d
Matheus <matheusteodorogarcia@gmail.com>
2024-05-11 11:28:59 -0300

    Protected Route

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Footer from './components/Footer';
 import Home from './components/Home';
 import Login from './components/Login/Login';
 import { UserStorage } from './UserContext';
+import User from './components/User/User';
+import ProtectedRoute from './components/Helper/ProtectedRoute';
 
 const App = () => {
   return (
@@ -15,6 +17,14 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login/*" element={<Login />} />
+          <Route
+            path="/conta/*"
+            element={
+              <ProtectedRoute>
+                <User />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
         <Footer />
       </UserStorage>
