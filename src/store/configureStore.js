2e67e7ce3cdc5225cfca0d63aef9b8f5caccbd2b
Matheus <matheusteodorogarcia@gmail.com>
2024-07-28 15:48:57 -0300

    Refactoring Feed and infinite scroll with Redux

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,9 +2,10 @@ import { configureStore } from '@reduxjs/toolkit';
 import photo from './photo';
 import token from './token';
 import user from './user';
+import feed from './feed';
 
 const store = configureStore({
-  reducer: { photo, token, user },
+  reducer: { photo, token, user, feed },
   middleware: (getDefaultMiddleware) => [...getDefaultMiddleware()],
 });
 
