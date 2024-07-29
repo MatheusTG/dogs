4259778ae2ae5d378600840c6c540c7447836cb0
Matheus <matheusteodorogarcia@gmail.com>
2024-07-28 21:41:19 -0300

    Refactoring PHOTO_POST with Redux

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,9 +4,10 @@ import token from './token';
 import user from './user';
 import feed from './feed';
 import ui from './ui';
+import photoPost from './photoPost';
 
 const store = configureStore({
-  reducer: { photo, token, user, feed, ui },
+  reducer: { photo, token, user, feed, ui, photoPost },
   middleware: (getDefaultMiddleware) => [...getDefaultMiddleware()],
 });
 
